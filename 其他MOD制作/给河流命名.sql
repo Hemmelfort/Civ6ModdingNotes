cb101d2007385d3180217c29cb48167522ff8170
Hemmelfort <hemmelfort@foxmail.com>
2019-07-11 20:16:14 +0800

    加入新mod：给河流命名

---
+++ 其他MOD制作/给河流命名.sql
@@ -0,0 +1,8 @@
+
+-- 仅支持《风云变幻》，而且是与文明相绑定
+
+INSERT INTO NamedRivers(NamedRiverType, Name) VALUES
+("River_Test", "一条大河");
+
+INSERT INTO NamedRiverCivilizations(NamedRiverType, CivilizationType) VALUES
+("River_Test", "CIVILIZATION_FZL");
