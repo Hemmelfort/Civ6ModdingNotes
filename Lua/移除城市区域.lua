a0be8228a9b22594bb520884da98b6c51254fa69
Hemmelfort <hemmelfort@foxmail.com>
2019-07-26 23:02:45 +0800

    添加 移除城市区域.lua

---
+++ Lua/移除城市区域.lua
@@ -0,0 +1,20 @@
+
+-- 通过项目移除城市区域（参考qqqbbb）
+-- 需要：
+--      先创建一个项目，项目中的 PrereqDistrict 为要移除的区域。
+
+function OnCityPrjComp(playerID, cityID, projectID, buildingIndex, iX, iY, bCancelled)
+    local sDistrictType = GameInfo.Projects[projectID].PrereqDistrict
+    
+    if sDistrictType ~= nil then
+        local pPlayer = Players[playerID]
+        local pCity = pPlayer:GetCities():FindID(cityID)
+        local tDistricts = pCity:GetDistricts()
+        local iDistrictID = GameInfo.Districts[sDistrictType].Index
+        
+        tDistricts:RemoveDistrict(iDistrictID)        
+    end    
+end
+
+Events.CityProjectCompleted.Add(OnCityPrjComp)
+
