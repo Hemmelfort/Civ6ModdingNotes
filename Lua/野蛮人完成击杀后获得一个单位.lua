94b9bc5e8133e8cb44438929315e5407a3541b03
Hemmelfort <hemmelfort@foxmail.com>
2019-08-26 21:53:51 +0800

    改了个bug：获取坐标出现-9999以致游戏强退

--- Lua/野蛮人完成击杀后获得一个单位.lua
+++ Lua/野蛮人完成击杀后获得一个单位.lua
@@ -1,4 +1,7 @@
 
+-- 当野蛮人将另一个单位击杀，则刷新出一个野蛮人。
+-- 尚无法根据被害单位的类型来决定新生单位的类型。
+
 function KilledByBarbarian(killedPlayerID, killedUnitID, PlayerID, UnitID)
     local pPlayer = Players[PlayerID]
     
@@ -7,9 +10,11 @@ function KilledByBarbarian(killedPlayerID, killedUnitID, PlayerID, UnitID)
         local sUnitType = GameInfo.Units[heroUnit:GetType()].UnitType
         local PlotX = heroUnit:GetX()
         local PlotY = heroUnit:GetY()
-                    
-        --pPlayer:GetUnits():Create(GameInfo.Units[heroUnit:GetType()].Index, PlotX, PlotY)
-        UnitManager.InitUnitValidAdjacentHex(PlayerID, sUnitType, PlotX, PlotY, 1)
+        
+        -- 有时候坐标会返回 -9999 导致游戏强退，所以要验证
+        if (PlotX > -1) and (PlotY > -1) then
+            UnitManager.InitUnitValidAdjacentHex(PlayerID, sUnitType, PlotX, PlotY, 1)
+        end
     end
     
 end
