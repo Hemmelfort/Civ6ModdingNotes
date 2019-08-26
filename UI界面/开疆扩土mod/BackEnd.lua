9bdb53639830815579d40c267d219ef3889516ee
Hemmelfort <hemmelfort@foxmail.com>
2019-08-26 22:56:01 +0800

    改进：把单元格纳入最近的城市

--- UI界面/开疆扩土mod/BackEnd.lua
+++ UI界面/开疆扩土mod/BackEnd.lua
@@ -1,9 +1,27 @@
 
+-- 计算距离最近的城市
+
+function GetNearestCity(PlotX, PlotY, PlayerID)
+    local dist = 9999
+    local city = nil
+    local pPlayerCities = Players[PlayerID]:GetCities()
+    
+    for i, pCity in pPlayerCities:Members() do
+        local iDistance = Map.GetPlotDistance(PlotX, PlotY, pCity:GetX(), pCity:GetY())
+        
+        if (iDistance < dist) then
+            dist = iDistance
+            city = pCity
+        end
+    end
+    
+    return city or pPlayerCities:GetCapitalCity()
+end
+
 
 function ChangePlotOwner(PlotX, PlotY, PlayerID)
     local pPlot = Map.GetPlot(PlotX, PlotY)
-    local pPlayer = Players[PlayerID]
-    local pCity = pPlayer:GetCities():GetCapitalCity()
+    local pCity = GetNearestCity(PlotX, PlotY, PlayerID)
     
     WorldBuilder.CityManager():SetPlotOwner(pPlot, pCity)
 end
